0ef9ed3128fa8d7afc3949c92d09f66515145c88
Ivan <ivanmoreno405@gmail.com>
2020-08-20 16:22:03 -0500

    i added the @CrossOrigin annotation to access the front-end

--- API_Rest/src/main/java/com/badbadcode/application/controllers/ClienteRestController.java
+++ API_Rest/src/main/java/com/badbadcode/application/controllers/ClienteRestController.java
@@ -1,13 +1,18 @@
 package com.badbadcode.application.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.badbadcode.application.model.entity.Cliente;
 import com.badbadcode.application.model.services.IClienteService;
-
+/*
+ * cros : intercambio de recursos de origen cruzado
+ * http://localhost:4200 ----> puerto en donde esta situado nuestro front-end
+ */
+@CrossOrigin(origins = {"http://localhost:4200"}) 
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
